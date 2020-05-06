efce0f4f0fb4f5ac51677b4514b9f40c76c92bba
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-06 17:23:12 -0600

    Complete deck iterations

---
+++ test/deck_test.rb
@@ -0,0 +1,49 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require "pry"
+
+class DeckTest < Minitest::Test
+
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @card4 = Card.new(:club, '5', 5)
+    cards = [@card1, @card2, @card3]
+    @deck = Deck.new(cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+
+  def test_it_has_cards
+    cards = [@card1, @card2, @card3]
+    assert_equal cards, @deck.cards
+  end
+
+  def test_rank_of_card_at
+    assert  12, @deck.rank_of_card_at(0)
+  end
+
+  def test_it_has_high_ranking_cards
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
+  end
+
+  def test_percent_high_raking
+    assert_equal 66.67, @deck.percent_high_ranking
+  end
+
+  def test_can_remove_cards
+    @deck.remove_card
+    assert_equal [@card2, @card3], @deck.cards
+  end
+
+  def test_it_can_add_cards
+    @deck.add_card(@card4)
+    assert_equal [@card1, @card2, @card3, @card4], @deck.cards
+  end
+end
