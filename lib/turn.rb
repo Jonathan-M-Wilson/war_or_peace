ee4b030f13699065a3e1bd5d8197162f1589b4a5
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-06 17:39:23 -0600

    Outline turn class

---
+++ lib/turn.rb
@@ -0,0 +1,34 @@
+class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @pile_cards = []
+    @spoils_of_war = []
+  end
+
+  def type
+    if
+
+  def spoils_of_war
+    @pile_cards
+  end
+
+
+
+
+
+  def calc_basic
+
+  end
+
+  def calc_war
+
+  end
+  def calc_mutually_assured_destruction
+
+  end
+end
