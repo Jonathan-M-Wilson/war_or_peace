732e8a692eaf58369aa1196c82cfceb40187a9aa
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-07 09:57:51 -0600

    Create other turn tests

---
+++ test/turn_mutually_assured_destruction_test.rb
@@ -0,0 +1,58 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
+require "pry"
+
+#mutually assured destruction
+class TurnMADTest < Minitest::Test
+
+  def setup
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    def test_it_exists
+      assert_instance_of Turn, @turn
+    end
+
+    def test_players_make_turns
+      assert_equal @player1, @turn.player1
+      assert_equal @player2, @turn.player2
+    end
+
+    def test_spoils_of_war_holds_cards
+      assert [], @turn.spoils_of_war
+    end
+
+    def test_type_of_turn
+      assert :mutually_assured_destruction, @turn.type
+    end
+
+    def test_it_has_winner
+      assert_equal @player1, @turn.winner
+    end
+
+    def test_it_can_pile_cards
+      assert_equal @turn.spoils_of_war, @turn.pile_cards
+    end
+
+    def test_it_awards_spoils_to_turn_winner
+      winner = @turn.winner
+      @turn.pile_cards
+      @turn.award_spoils(winner)
+      assert_equal 5, winner.deck.cards.count
+    end
+  end
