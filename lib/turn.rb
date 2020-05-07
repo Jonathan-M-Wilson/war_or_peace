a197639398137c650cd91b1c1717e2bc41a4163d
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-07 16:32:07 -0600

    Arrange by importance

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,22 +7,20 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @type = type
     @winner = winner
   end
 
   def type
     if @player1.deck.rank_of_card_at(0) !=
-       @player2.deck.rank_of_card_at(0)
+      @player2.deck.rank_of_card_at(0)
       :basic
     elsif @player1.deck.rank_of_card_at(0) ==
-          @player2.deck.rank_of_card_at(0)
-      :war
-    elsif @player1.deck.rank_of_card_at(0) !=
-          @player2.deck.rank_of_card_at(0) &&
-          @player1.deck.rank_of_card_at(2) ==
-          @player2.deck.rank_of_card_at(2)
+      @player2.deck.rank_of_card_at(0) &&
+      @player1.deck.rank_of_card_at(2) ==
+      @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
+    else
+      :war
     end
   end
 
