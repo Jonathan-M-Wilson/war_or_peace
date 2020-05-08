216e8f27a20df3ef48cfe2253972b5cdc56b41e0
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-08 08:33:56 -0600

    Finish reformat and iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -59,5 +59,3 @@ class Turn
     end
   end
 end
-
-#Add Comments
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,30 +8,16 @@ require "pry"
 
 class TurnBasicTest < Minitest::Test
 
-  def setup
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    card9 = Card.new(:diamond, "9", 9)
 
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
+  #Index
+  #test_it_exists
+  #test_players_make_turns
+  #test_spoils_of_war_holds_cards
+  #test_it_has_type
+  #test_it_has_winner
+  #test_it_can_pile_cards
+  #test_it_can_award_spoils
 
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-
-    turn = Turn.new(player1, player2)
-
-
-
-
-
-  end
 
   def test_it_exists
     card1 = Card.new(:heart, 'Jack', 11)
@@ -123,11 +109,10 @@ class TurnBasicTest < Minitest::Test
     player2.deck.rank_of_card_at(0)
 
     assert_equal :basic, turn.type
-    skip
+
   end
 
   def test_it_has_winner_basic
-
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -147,6 +132,56 @@ class TurnBasicTest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal player1, turn.winner
+  end
+
+  def test_it_can_pile_cards_when_type_basic
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    card9 = Card.new(:diamond, "9", 9)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+
+    assert_equal turn.spoils_of_war, turn.pile_cards
+  end
+
+  def test_it_awards_spoils_to_turn_winner_type_basic
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    card9 = Card.new(:diamond, "9", 9)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal 5, winner.deck.cards.count
   end
 
   def test_it_has_type_of_war
@@ -172,7 +207,6 @@ class TurnBasicTest < Minitest::Test
   end
 
   def test_it_has_winner_war
-
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -194,8 +228,57 @@ class TurnBasicTest < Minitest::Test
     assert_equal player1, turn.winner
   end
 
-  def test_it_has_type_mad
+  def test_it_can_pile_cards_when_type_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    card9 = Card.new(:diamond, "9", 9)
 
+    war_deck1 = Deck.new([card1, card2, card3, card5])
+    war_deck2 = Deck.new([card4, card6, card7, card8])
+
+    player1 = Player.new("Megan", war_deck1)
+    player2 = Player.new("Aurora", war_deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+
+    assert_equal turn.spoils_of_war, turn.pile_cards
+  end
+
+  def test_it_awards_spoils_to_turn_winner_type_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    card9 = Card.new(:diamond, "9", 9)
+
+    war_deck1 = Deck.new([card1, card2, card3, card5])
+    war_deck2 = Deck.new([card4, card6, card7, card8])
+
+    player1 = Player.new("Megan", war_deck1)
+    player2 = Player.new("Aurora", war_deck2)
+
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal 7, winner.deck.cards.count
+  end
+
+  def test_it_has_type_mad
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -218,7 +301,6 @@ class TurnBasicTest < Minitest::Test
   end
 
   def test_it_has_winner_mad
-
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -241,16 +323,53 @@ class TurnBasicTest < Minitest::Test
     assert_equal "No Winner", turn.winner
   end
 
-  def test_it_can_pile_cards
-    skip
+  def test_it_can_pile_cards_when_type_mad
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    card9 = Card.new(:diamond, "9", 9)
+
+    mad_deck1 = Deck.new([card1, card2, card3, card5])
+    mad_deck2 = Deck.new([card4, card6, card9, card8])
+
+    player1 = Player.new("Megan", mad_deck1)
+    player2 = Player.new("Aurora", mad_deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+
     assert_equal turn.spoils_of_war, turn.pile_cards
   end
 
-  def test_it_awards_spoils_to_turn_winner
-    skip
+  def test_it_awards_no_spoils_to_turn_winner_type_mad
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    card9 = Card.new(:diamond, "9", 9)
+
+    mad_deck1 = Deck.new([card1, card2, card3, card5])
+    mad_deck2 = Deck.new([card4, card6, card9, card8])
+
+    player1 = Player.new("Megan", mad_deck1)
+    player2 = Player.new("Aurora", mad_deck2)
+
+    turn = Turn.new(player1, player2)
     winner = turn.winner
     turn.pile_cards
     turn.award_spoils(winner)
-    assert_equal 5, winner.deck.cards.count
+
+    assert_equal [card5], player1.deck.cards
+    assert_equal [card8], player2.deck.cards
   end
 end
